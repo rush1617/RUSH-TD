2bdc452c284c0a85bbf7006d56fbcab80d8e9db3
rush1617 <rushtd1617@gmail.com>
2025-07-07 19:47:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -102,8 +102,9 @@ async function connectToWA() {
 └──────── ⋆⋅☆⋅⋆ ────────┘
 
 🚀 Powered By *RAMESH*
-*DISSANAYAKA* 🔥 `;
-      await rush.sendMessage(ownerNumber[0] + "@s.whatsapp.net", {
+*DISSANAYAKA* 🔥
+       `;
+      conn.sendMessage(ownerNumber[0] + "@s.whatsapp.net", {
         image: { url: 'https://github.com/rush1617/RUSH-TD/blob/main/images/Rush-TD%20(1).png?raw=true' },
         caption: up
       });
