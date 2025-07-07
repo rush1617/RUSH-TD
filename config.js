04d66bc2abd07d0e94641661e20457d2dd913262
rush1617 <rushtd1617@gmail.com>
2025-07-07 18:02:03 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-  SESSION_ID: "", // Put your session id here
+  SESSION_ID: "fNEmAKoB#mb_Z_WAI4bZ5yRoXADwC73UDzcQdsCMBRiC7CDYOhe8", // Put your session id here
   ALIVE_IMG: "https://github.com/rush1617/RUSH-TD/blob/main/images/Rush-TD%20(1).png?raw=true",
   ALIVE_MSG: "*Hello👋 RUSH-TD Is Alive Now😍*", // Change alive msg from here
   BOT_OWNER: "94726892483", // Replace your bot owner number here with 94(country code)
