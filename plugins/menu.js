fc1e89358063d4f01b96330bcae6e6217653c87a
rush1617 <rushtd1617@gmail.com>
2025-07-11 15:27:50 +0530

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -3,6 +3,7 @@ const { cmd, commands } = require("../command");
 cmd(
   {
     pattern: "menu",
+    react: "⚙️",
     desc: "Displays all available commands",
     category: "main",
     filename: __filename,
