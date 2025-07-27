65220ae1526f1b3f417a833f4e2c2c1476495fc1
RUSH-TD <rushtd1617@gmail.com>
2025-07-27 23:03:04 +0530

    Update fb.js

--- plugins/fb.js
+++ plugins/fb.js
@@ -5,7 +5,7 @@ cmd(
   {
     pattern: "fb",
     alias: ["fb", "facebook"],
-    react: "✅",
+    react: "📘",
     desc: "Download Facebook Video",
     category: "download",
     filename: __filename,
