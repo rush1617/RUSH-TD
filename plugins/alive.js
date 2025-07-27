fdfbf618180316b6887068516c2883ebb681d220
RUSH-TD <rushtd1617@gmail.com>
2025-07-27 23:46:02 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -14,8 +14,6 @@ const formatUptime = (seconds) => {
 cmd({
     pattern: "alive",
     react: "👀",
-    desc: "Check if the bot is online and functioning.",
-    category: "main",
     filename: __filename
 },
 async (conn, mek, m, {
