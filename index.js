ec245356b4dbd801250a40aa31f492f6ab41f255
RUSH-TD <rushtd1617@gmail.com>
2025-08-29 12:51:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const app = express();
 const port = process.env.PORT || 8000;
 
 const prefix = '.';
-const ownerNumber = ['94726892483'];
+const ownerNumber = ['94775938007'];
 const credsPath = path.join(__dirname, '/auth_info_baileys/creds.json');
 
 async function ensureSessionFile() {
