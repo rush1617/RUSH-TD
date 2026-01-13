ea7d0c383bf6ecf9407e6280ed42e72ef2c7e0f1
RUSH-TD <rushtd1617@gmail.com>
2026-01-13 11:45:02 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-  SESSION_ID: "ndkSjTII#kbqbx-fUckuYyIif4AL2FIq6RY1Wth1bjDhO_waMPRM", // Put your session id here
+  SESSION_ID: "6AtB0ACT#1qtQEgiZ6uvMvREyUxUVJKQlARKV8qXTUUJqyPxD-r8", // Put your session id here
   ALIVE_IMG: "https://github.com/rush1617/RUSH-TD/blob/main/images/Alive.png?raw=true",
   ALIVE_MSG: `╔═══◉ *🟢 STATUS: ONLINE* ◉═══╗
 ║  𝙷𝚎𝚢 𝙳𝚞𝚍𝚎, 𝙸’𝚖 𝚑𝚎𝚛𝚎 𝚝𝚘 𝚑𝚎𝚕𝚙 𝚢𝚘𝚞.  
