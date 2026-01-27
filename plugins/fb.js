aa54e116682cda1d63c0716967e1a0d8a8fb5da3
RUSH-TD <rushtd1617@gmail.com>
2026-01-27 10:18:28 +0530

    Update fb.js

--- plugins/fb.js
+++ plugins/fb.js
@@ -60,14 +60,14 @@ cmd(
       const desc = 
         `🌟 𝗪𝗘𝗟𝗖𝗢𝗠𝗘 𝗧𝗢 🌟 \n\n` +   
        `═════════════════════ \n` +
-      `🔮  R U S H - T D  🔮 \ n`
-`✅ *FACEBOOK VIDEO DOWNLOADER* ✅  \n`
-       ` ═════════════════════   \n`
-   `🚀 Pow. By *RAMESH DISSANAYAKA* 🔥\n`
-        `──────────────────────\n`
-   `👻 *Title*: ${title || "Unknown"}\n`
-   `👻 *Quality*: ${qualityText}\n`
-        `──────────────────────\n`
+      `🔮  R U S H - T D  🔮 \ n` +
+`✅ *FACEBOOK VIDEO DOWNLOADER* ✅  \n` +
+       ` ═════════════════════   \n` +
+   `🚀 Pow. By *RAMESH DISSANAYAKA* 🔥\n` +
+        `──────────────────────\n` +
+   `👻 *Title*: ${title || "Unknown"}\n` +
+   `👻 *Quality*: ${qualityText}\n` +
+        `──────────────────────\n` +
     `🎼 Made with ❤️ by RAMESH DISSANAYAKA💫\n`;
 
       await rush.sendMessage(
