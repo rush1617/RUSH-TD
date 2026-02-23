39c2d4b78e0ff7018d8de50beb753bc6e62d9922
RUSH-TD <rushtd1617@gmail.com>
2026-02-23 11:17:03 +0530

    Create save.js

---
+++ plugins/save.js
@@ -0,0 +1,17 @@
+const { getContentType } = require('@adiwajshing/baileys');
+
+const savePlugin = async (msg, { sendMessage }) => {
+  if (msg.body.startsWith('.save')) {
+    const content = await msg.download();
+    if (!content) return;
+
+    const type = getContentType(msg.message);
+    const mediaType = type.replace('Message', '');
+
+    // Save logic here (e.g., save to local storage or cloud)
+    console.log(`Saving ${mediaType}...`);
+    // Example: writeFileSync(`./saved/${Date.now()}.${mediaType}`, content);
+  }
+};
+
+module.exports = savePlugin;
