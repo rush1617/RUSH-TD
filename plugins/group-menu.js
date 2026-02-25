a18fa8bb52c88ce08bb63eb6429d7522fc6ffe78
RUSH-TD <rushtd1617@gmail.com>
2026-02-25 12:56:50 +0530

    Update group-menu.js

--- plugins/group-menu.js
+++ plugins/group-menu.js
@@ -3,7 +3,7 @@ const { cmd } = require("../command");
 cmd(
   {
     pattern: "group",
-    react: "📥",
+    react: "👥",
     filename: __filename,
   },
   async (rush, mek, m, { from, reply }) => {
