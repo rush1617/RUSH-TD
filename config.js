acc3cbc6ed9852248ff74688baa228094f116293
RUSH-TD <rushtd1617@gmail.com>
2026-07-19 12:02:20 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-  SESSION_ID: "rBsVQABD#7le5xZ9RMFxUPAqgUmylXDMU-6sYtKlC4paASRHYZnk", // Put your session id here
+  SESSION_ID: "uI9g0KpI#NHhquTCzmYiHkPl0BGm6H8vEzEvvw4zIvSLK-cF0sHI", // Put your session id here
   ALIVE_IMG: "https://github.com/rush1617/RUSH-TD/blob/main/images/Alive.png?raw=true",
   ALIVE_MSG: `╔═══◉ *🟢 STATUS: ONLINE* ◉═══╗\n` +
 `║  𝙷𝚎𝚢 𝙳𝚞𝚍𝚎, 𝙸’𝚖 𝚑𝚎𝚛𝚎 𝚝𝚘 𝚑𝚎𝚕𝚙 𝚢𝚘𝚞. \n` +
